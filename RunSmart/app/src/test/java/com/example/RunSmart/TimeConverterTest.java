ec36407889b356e125e79ab8434d89ded56a3a8a
Jonah <100175129+J-onah@users.noreply.github.com>
2022-05-27 22:37:07 +0800

    Add files via upload

---
+++ RunSmart/app/src/test/java/com/example/RunSmart/TimeConverterTest.java
@@ -0,0 +1,19 @@
+package com.example.RunSmart;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class TimeConverterTest extends TestCase {
+
+    @Test
+    public void testConvertMinutesToHourMinutes() {
+        int input = 80;
+        String output;
+        String expected = "1:20";
+        TimeConverter timeConverter = new TimeConverter();
+        output = timeConverter.convertMinutesToHourMinutes(input);
+
+        assertEquals(expected, output);
+    }
+}
