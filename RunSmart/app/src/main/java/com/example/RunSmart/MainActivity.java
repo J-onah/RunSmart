22f9fc52a76fe33879141010319606437a1c784c
Jonah <100175129+J-onah@users.noreply.github.com>
2022-05-28 01:21:31 +0800

    Update MainActivity.java

--- RunSmart/app/src/main/java/com/example/RunSmart/MainActivity.java
+++ RunSmart/app/src/main/java/com/example/RunSmart/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 String formattedDate = df.format(c);
 
                 if(Text.isEmpty()){
-                    mLinearLayout.setBackgroundResource(R.drawable.rain);
+                    mLinearLayout.setBackgroundResource(R.drawable.main);
                     alert("Please enter a valid input!");
                 }
                 else {
